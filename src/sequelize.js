35871e78764ea8a91930423f6a6d1e2ee4bb8d4a
Daniel Alves <danialves2103@gmail.com>
2023-08-26 15:16:37 -0400

    asdasdada

--- src/sequelize.js
+++ src/sequelize.js
@@ -1,13 +1,13 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-    'postgres', // Nome do banco de dados
+    'teste', // Nome do banco de dados
     'postgres', // Nome de usuário
-    'TzDujLtwyy6dCdrPAgvp', // Senha
+    'gWKbaPzMMalfTpvg7KxG', // Senha
     {
         dialect: 'postgres', // Dialet do banco de dados
-        host: 'containers-us-west-163.railway.app', // Host do banco de dados
-        port: 7249, // Porta do banco de dados
+        host: 'containers-us-west-66.railway.app', // Host do banco de dados
+        port: 6970, // Porta do banco de dados
         logging: false
     }
 );
